28b950e8c8581d33ec52a4d96ea30dd4ae0b5091
NidhalBousalem <boussalemnidhal@gmail.com>
2022-05-29 10:14:20 +0100

    Did some changes !

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -26,6 +26,25 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
+
+    private static final int[] shiftNum = {1,3,7,15,31,63,127,255};
+    public static final String MARKER = "@RM";
+    public static final int HEADER_LENGTH = MARKER.length() * 8 + 32 + 32 + 8 + 16 + 8 + 8;
+    private int[] dataHeader = new int[HEADER_LENGTH];
+
+
+
+
+
+
+
+
+
+
+
     TextView titleTextView;
     TextView titleTextView2;
     TextView durationTextView;
@@ -291,16 +310,172 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO: Implements the watermarking methods here:
 
-    public void applyWatermark(Uri imageUri, Uri audioUri) {
+    public int[] applyWatermark(int[] audioSamples, byte[] message) {
+
+        int LSBUSed = 1; // Integer.parseInt((String)this.properties.get("lsb"));
+        int shiftNumber = this.shiftNum[LSBUSed-1];
+        int[] newSample = new int[audioSamples.length];
+        int offset = 0;
+
+        // write marker to audio samples
+        for (int i=0;i<MARKER.length();i++){
+            int kar = MARKER.charAt(i);
+            for (int j=0;j<8;j++){
+                int bitExtract = kar & 1;
+                newSample[offset] = audioSamples[offset] & ~1;
+                newSample[offset] |= bitExtract;
+                kar >>= 1;
+                offset++;
+            }
+        }
+        // end write marker --- next offset = 24
+
+
+
+
+        // write length message to samples
+        int lengthMessage = (int) Math.ceil((double)message.length * 8 /(double) LSBUSed);
+        int length2 = lengthMessage;
+        for (int i=0;i<32;i++){
+            int bitExtract = lengthMessage & 1;
+            newSample[offset] = audioSamples[offset] & ~1;
+            newSample[offset] |= bitExtract;
+            lengthMessage >>= 1;
+            offset++;
+        }
+        // end write length message --- next offset = 56
+        //System.out.println("next:" + offset);
+
+
+
+
+
+        // write message
+        int realLength = 0;
+        int byteExtract = 0;
+        int bitRem=0;
+        int bitDiambil = 0;
+        int i = 0;
+        int lastBit = (message.length * 8) % LSBUSed;
+
+        while (i<message.length){
+            int bitExtract = message[i] & 1;
+            message[i] >>= 1;
+            bitDiambil++;
+
+            byteExtract |= bitExtract << bitRem;
+
+            bitRem++;
+
+            if (bitDiambil >= 8){
+                i++;
+                bitDiambil = 0;
+                realLength++;
+            }
+
+            if (bitRem >=LSBUSed){
+                newSample[offset] = audioSamples[offset] & ~shiftNumber;
+                newSample[offset] |= byteExtract;
+                offset++;
+                byteExtract = 0;
+                bitRem = 0;
+            }else if(bitRem == lastBit && (offset-(HEADER_LENGTH-1)) == length2) {
+                newSample[offset] = audioSamples[offset] & ~ this.shiftNum[lastBit];
+                newSample[offset] |= byteExtract;
+                offset++;
+                byteExtract = 0;
+                bitRem = 0;
+                i++;
+                bitDiambil=0;
+            }
+
+            if (offset >=audioSamples.length){
+                break;
+            }
+        }
+
+        for (int k=offset;k<audioSamples.length;k++){
+            newSample[k] = audioSamples[k];
+        }
+
+        offset = 24;
+        //System.out.println("len:" + realLength);
+        int realLen = (int) Math.ceil((double)realLength * 8 /(double) LSBUSed);
+        for (int j=0;j<32;j++){
+            int bitExtract = realLen & 1;
+            newSample[offset] = newSample[offset] & ~1;
+            newSample[offset] |= bitExtract;
+            realLen >>= 1;
+            offset++;
+        }
+
+        return newSample;
 
     }
 
-    public void extractWatermark(Uri audioUri) {
+    public byte[] extractWatermark(int[] audioSamples) {
+
+        int LSBUSed = 1; // Integer.parseInt((String)this.properties.get("lsb"));
+
+        int byteExtract = 0;
+        int bitDiambil=0;
+
+
+        int startIndex = 128;
+
+
+        // Testing with 10000
+        int numByte = 10000; // Integer.parseInt((String) this.properties.get("msgSize"));//lengthMessage * LSBUSed / 8;
+        byte[] message = new byte[numByte];
+
+        int i = 0;
+        int bitRem = 0;
+
+        while(i<numByte){
+            int bitExtract = audioSamples[startIndex] & 1;
+            audioSamples[startIndex] >>=1;
+            bitDiambil++;
+            byteExtract |= bitExtract << bitRem;
+            bitRem++;
+
+            if (bitDiambil >=LSBUSed){
+                startIndex++;
+                bitDiambil = 0;
+            }
+            if (bitRem >=8){
+                message[i] = (byte) byteExtract;
+                i++;
+                bitRem = 0;
+                byteExtract = 0;
+            }
+
+            if (startIndex >= audioSamples.length)
+                break;
+
+        }
+        return message;
 
     }
 
     public boolean analyzeWatermark(Uri audioUri) {
-        return false;
+        byte[] dataHead = new byte[MARKER.length()];
+
+        int startIndex = 0;
+        for (int i=0;i<dataHead.length;i++){
+            int charEx = 0;
+            for (int j=0;j<8;j++){
+                int bitExtract = this.dataHeader[startIndex] & 1;
+                charEx |= bitExtract << j;
+                startIndex++;
+            }
+            dataHead[i] = (byte) charEx;
+        }
+        String head = new String(dataHead);
+        System.out.println(head);
+        if (head.equals(MARKER))
+            return true;
+        else
+            return false;
     }
 
 }
